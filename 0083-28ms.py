69bcad71f96a25f6674a01ee729b9ea4698e64d3
Square Tsou <zhanary@icloud.com>
2018-11-19 17:05:21 +0800

    Create 0083-28ms.py

---
+++ 0083-28ms.py
@@ -0,0 +1,18 @@
+In Python, every value is a reference, you can say a pointer, to an object. Objects cannot be values. Assignment always copies the value; two such pointers point to the same object.
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def deleteDuplicates(self, head):
+        cur = head
+        while cur:
+            while cur.next and cur.next.val == cur.val:
+                cur.next = cur.next.next
+            cur = cur.next
+        return head
+
