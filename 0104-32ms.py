6af67d55d4339fb51034f7de5d19bdbfcbfc4f69
Square Tsou <zhanary@icloud.com>
2018-11-22 17:56:00 +0800

    Create 0104-32ms.py

---
+++ 0104-32ms.py
@@ -0,0 +1,14 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def maxDepth(self, root):
+        if root:
+            return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        else:
+            return 0  
+
