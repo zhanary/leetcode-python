ed8dce4646d0f7be3d40ff491bc5c68ca1b96446
Square Tsou <zhanary@icloud.com>
2018-12-02 17:00:11 +0800

    Create 0110-40ms.py

---
+++ 0110-40ms.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def isBalanced(self, root):
+        def check(root):
+            if root is None:
+                return 0
+            left = check(root.left)
+            right = check(root.right)
+            if left == -1 or right == -1 or abs(left-right) > 1: #abs返回绝对值
+                return -1
+            return 1+max(left, right)
+        
+        return check(root) != -1
+        
