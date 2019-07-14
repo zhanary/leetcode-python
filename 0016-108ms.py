ae37c79f226517a870e697ea28967088accb2109
Square Tsou <zhanary@icloud.com>
2019-07-14 21:42:12 +0800

    Update and rename 0016-304ms.py to 0016-108ms.py

--- 0016-108ms.py
+++ 0016-108ms.py
@@ -6,21 +6,25 @@ class Solution(object):
         :rtype: int
         """
         nums.sort()    #先排序
-        ans = None
+        
+        ans = None  #当前最优
 
         for i in range(len(nums)):
             left = i + 1        #数字i的右边
             right = len(nums) - 1
             
             while left < right:   #非常重要防止越界
-                thsum = nums[i] + nums[left] + nums[right]
-
+                thsum = nums[i] + nums[left] + nums[right]   #总和            
+                
                 if ans == None or abs(thsum - target) < abs(ans - target): #更好的sum值更新
                     ans = thsum 
 
                 if thsum > target:
                     right -= 1
-                else:
+                elif thsum < target:
                     left +=1
+                else:
+                    return target
             
         return ans
+                
